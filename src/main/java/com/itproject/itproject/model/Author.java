e5a2d041980e6c64e4d7dd00d653bfaf14bb741a
Bryan Portillo <bportillo701@gmail.com>
2023-09-01 17:08:13 -0600

    creating Author model

---
+++ src/main/java/com/itproject/itproject/model/Author.java
@@ -0,0 +1,64 @@
+package com.itproject.itproject.model;
+
+import java.sql.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class Author {
+
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @Temporal(TemporalType.DATE)
+  private Date birthDate;
+
+  private String country;
+
+
+  public Long getId() {
+    return id;
+  }
+
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+
+  public String getCountry() {
+    return country;
+  }
+
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+}
