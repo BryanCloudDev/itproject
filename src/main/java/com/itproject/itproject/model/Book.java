d0215b1b1513fea27d41d7c2d8b71d54af9f330a
Bryan Portillo <bportillo701@gmail.com>
2023-09-01 17:41:45 -0600

    Creating Book Model

---
+++ src/main/java/com/itproject/itproject/model/Book.java
@@ -0,0 +1,74 @@
+package com.itproject.itproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+  
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String author;
+
+  private String category;
+
+  private double price;
+
+  private String status;
+
+  /* Getter and setters */
+
+  public Long getId() {
+      return id;
+  }
+
+  public void setId(Long id) {
+      this.id = id;
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public void setName(String name) {
+      this.name = name;
+  }
+
+  public String getAuthor() {
+      return author;
+  }
+
+  public void setAuthor(String author) {
+      this.author = author;
+  }
+
+  public String getCategory() {
+      return category;
+  }
+
+  public void setCategory(String category) {
+      this.category = category;
+  }
+
+  public double getPrice() {
+      return price;
+  }
+
+  public void setPrice(double price) {
+      this.price = price;
+  }
+
+  public String getStatus() {
+      return status;
+  }
+
+  public void setStatus(String status) {
+      this.status = status;
+  }
+}
