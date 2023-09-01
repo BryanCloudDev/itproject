ee9c2bad0353cd5ae145a0cc68532e5c7a692c3e
Bryan Portillo <bportillo701@gmail.com>
2023-09-01 17:08:41 -0600

    Creating Author Service

---
+++ src/main/java/com/itproject/itproject/service/AuthorService.java
@@ -0,0 +1,45 @@
+package com.itproject.itproject.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itproject.itproject.model.Author;
+import com.itproject.itproject.repository.AuthorRepository;
+
+@Service
+public class AuthorService {
+
+  @Autowired
+  private final AuthorRepository authorRepository;
+
+  public AuthorService(AuthorRepository authorRepository) {
+    this.authorRepository = authorRepository;
+  }
+
+  public Author createAuthor(Author author) {
+    return authorRepository.save(author);
+  }
+
+  public Author getAuthorById(Long id) {
+    return authorRepository.findById(id).orElse(null);
+  }
+
+    public List<Author> getAllAuthors() {
+    return authorRepository.findAll();
+  }
+
+  public Author updateAuthor(Long id, Author updatedAuthor) {
+    return authorRepository.findById(id).map(author -> {
+      author.setName(updatedAuthor.getName());
+      author.setBirthDate(updatedAuthor.getBirthDate());
+      author.setCountry(updatedAuthor.getCountry());
+      return authorRepository.save(author);
+    }).orElse(null);
+  }
+
+  public void deleteAuthor(Long id) {
+    authorRepository.deleteById(id);
+  }
+}
