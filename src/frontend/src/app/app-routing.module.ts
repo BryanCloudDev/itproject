93f7e97322c51bce5cfdb7e511fd4aa91db27458
Bryan Portillo <bportillo701@gmail.com>
2023-09-02 02:47:32 -0600

    adding add books component to app module

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { BookAdministrationComponent } from './book-administration/book-administration.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'book-administration', component: BookAdministrationComponent },
+  { path: 'add-books', component: AddBookComponent },
 ];
 
 @NgModule({
